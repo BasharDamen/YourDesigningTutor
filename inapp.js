4ab5d7736256790215e84223a17ece375d0ac1bb
BasharAlDamen <>
2021-06-02 14:13:23 +0000

    added js file

---
+++ inapp.js
@@ -0,0 +1,24 @@
+var firstName = "Bashar "
+var lastName = 'Al-Damen'
+
+var finalName = 'Developer:' + firstName + lastName
+
+console.log(finalName)
+
+alert("Hello, we are glad to see you")
+
+var UserAnswer = prompt("Are you a designer?")
+
+if (UserAnswer == "yes") {
+  var UserAnswer = prompt("Share with us your portofolio link")
+}
+else if (UserAnswer == "no") {
+  prompt("What is Your learning background?")
+}
+else {
+  alert("take a tour in our website")
+
+}
+
+
+document.write('<dic>' + 'Creativity is nothing but a mind set free' + '  -Torrie T. Asai' + '</div>')
