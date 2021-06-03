f1e219d5409415e7c0b21b30f81da50dc2a97f44
BasharAlDamen <>
2021-06-03 13:28:59 +0000

    addddd

--- inapp.js
+++ inapp.js
@@ -16,10 +16,10 @@ UserInput = prompt("rate our website from 1 - 10")
 
 var i = prompt ("how many years of excperience to you have in designing?")
 for (var x = 0 ; x < i ; x++){ 
-document.write('<img src=s7.jpg height=100px width=200 px>')
+console.log('Designing' + x )
+document.write('<img src=s7.jpg height=100px width=200px />')
 }
 console.log('Designing' + x )
 
 document.write('<div>' + '<h4> Creativity is nothing but a mind set free<h4>' + '  -Torrie T. Asai' + '</div>')
-}
 
