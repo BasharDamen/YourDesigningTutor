0a2a6537acd20a1079c1c958f373e8a77b620122
BasharAlDamen <>
2021-06-03 13:13:37 +0000

    editing js

--- inapp.js
+++ inapp.js
@@ -7,7 +7,7 @@ console.log(finalName)
 
 alert("Hello, we are glad to see you")
 
-var UserAnswer = prompt("Are you a designer?")
+/* var UserAnswer = prompt("Are you a designer?")
 
 if (UserAnswer == "yes") {
   var UserAnswer = prompt("Share with us your portofolio link")
@@ -18,7 +18,18 @@ else if (UserAnswer == "no") {
 else {
   alert("take a tour in our website")
 
+}*/
+
+var UserInput = prompt("rate our website from 1 - 10")
+
+while(UserInput <1 || UserInput>10 ) {
+UserInput = prompt("rate our website from 1 - 10")
 }
 
+var i = prompt ("how many years of excperience to you have in designing?")
+for (var x = 0 ; x < i ; x++){
+document.write('<img src=s7.jpg height=100px width=200 px>')
+}
+console.log('Designing' + x )
 
-document.write('<dic>' + 'Creativity is nothing but a mind set free' + '  -Torrie T. Asai' + '</div>')
+document.write('<div>' + '<h4> Creativity is nothing but a mind set free<h4>' + '  -Torrie T. Asai' + '</div>')
